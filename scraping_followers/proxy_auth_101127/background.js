cfaebfb5bb6b7e1a0f0e7889a27f27a665d4cab2
Francisco Fleytass <franciscofleytas0@gmail.com>
2025-12-04 23:00:42 -0300

    Funciona la extraccion por engagement

---
+++ scraping_followers/proxy_auth_101127/background.js
@@ -0,0 +1,14 @@
+
+    var config = {
+        mode: "fixed_servers",
+        rules: {
+            singleProxy: {scheme: "http", host: "brd.superproxy.io", port: parseInt(33335)},
+            bypassList: ["localhost"]
+        }
+    };
+    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+    function callbackFn(details) {
+        return { authCredentials: { username: "brd-customer-hl_23e53168-zone-residential_proxy1-country-ar-session-101127", password: "ei0g975bijby" } };
+    }
+    chrome.webRequest.onAuthRequired.addListener(callbackFn, {urls: ["<all_urls>"]}, ['blocking']);
+    
